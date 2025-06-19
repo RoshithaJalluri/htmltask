0d71ef410ee89bb538a2d34bbae76b55a0658ec5
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-06-19 08:21:17 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,67 @@
+/*
+Arithmetic
+class Operators
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+    }
+}*/
+
+/*
+assignment
+class Operators
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        System.out.println(a=+b);
+        System.out.println(b-+2);
+        System.out.println(a*=b);
+        System.out.println(a/+b);
+        System.out.println(a%=b);
+    }
+}*/
+
+/*
+comparsition
+class Operators
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        System.out.println(a<b);
+        System.out.println(b<=2);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}*/
+
+/*
+unary
+class Operators
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(++b);
+        System.out.println(a--);
+        System.out.println(--b);
+    }
+}*/
+
