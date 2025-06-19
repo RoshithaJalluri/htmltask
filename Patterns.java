2ec1b69369ff3c54936c7858aea31a6548b00eb2
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-06-19 11:59:09 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -1,16 +1,14 @@
-
-/*
-//single star
+/* single star
 public class Patterns
 {
 public static void main(String[]args)
 {
 System.out.println("*");
 }
-}
+}*/
 
 
-//vertical stars
+/*vertical stars
 public class Patterns
 {
 public static void main(String[]args)
@@ -20,10 +18,10 @@ for(int i=0;i<5;i++)
 System.out.println("*");
 }
 }
-}
+}*/
 
 
-//Horizontal stars
+/*Horizontal stars
 public class Patterns
 {
 public static void main(String[]args)
@@ -33,7 +31,7 @@ for(int i=0;i<5;i++)
 System.out.print("*");
 }
 }
-}
+}*/
 
 //Basic square pattern
 public class Patterns
@@ -52,11 +50,6 @@ System.out.println("");
 }
 
 /*square number patterns
-1 1 1 1 1 
-2 2 2 2 2 
-3 3 3 3 3 
-4 4 4 4 4 
-5 5 5 5 5*/
 public class Patterns
 {
 public static void main(String[]args)
@@ -70,14 +63,9 @@ System.out.print(i);
 System.out.println("");
 }
 }
-}
+}*/
 
 /*square numbers in horizontal way
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5*/
 public class Patterns
 {
 public static void main(String[]args)
@@ -91,14 +79,9 @@ System.out.print(j);
 System.out.println("");
 }
 }
-}
+}*/
 
 /*square continous number pattern
-1 2 3 4 5
-6 7 8 9 10
-11 12 13 14 15
-16 17 18 19 20
-21 22 23 24 25*/
 public class Patterns
 {
     public static void main(String[]args)
@@ -116,13 +99,10 @@ public class Patterns
             System.out.println();
         }
     }
-}
+}*/
+
+
 /* square patterns with 1 to 9 numbers
-1 2 3 4 5 
-6 7 8 9 1 
-2 3 4 5 6 
-7 8 9 1 2 
-3 4 5 6 7*/
 public class Patterns
 {
     public static void main(String[]args)
@@ -145,14 +125,9 @@ public class Patterns
 
         }
     }
-}
+}*/
 
 /* square patterns with incrementing number by 2
-1 3 5 7 9
-11 13 15 17 19
-21 23 25 27 29
-31 33 35 37 39
-41 43 45 47 49*/
 public class Patterns
 {
     public static void main(String[]args)
@@ -171,14 +146,9 @@ public class Patterns
 
         }
     }
-}
+}*/
 
-/*snumber pattern in square formate 
-0 1 0 1
-0 1 0 1
-0 1 0 1
-0 1 0 1
-0 1 0 1*/
+/*number pattern in square formate 
 public class Patterns
 {
     public static void main(String[]args)
@@ -192,16 +162,10 @@ public class Patterns
             System.out.println("");
         }
     }
-}
+}*/
 
 
-/*
-    *
-   ***
-  *****
- *******
-*********
-*/
+/*Triangle
 class Patterns
 {
     public static void main(String[]args)
@@ -222,17 +186,10 @@ class Patterns
             System.out.println();
         }
     }
-}
+}*/
 
 
-/*
-     2 
-   2 4 
-  2 4 6 
- 2 4 6 8 
-2 4 6 8 10
-
-*/
+/*Multiples of 2
 class Patterns
 {
     public static void main(String[]args)
@@ -253,17 +210,10 @@ class Patterns
             System.out.println();
         }
     }
-}
+}*/
 
 
-/*
-    * 
-   * * 
-  * * * 
- * * * * 
-* * * * * 
-
-*/
+/*Triangle
 class Patterns
 {
     public static void main(String[]args)
@@ -282,4 +232,115 @@ class Patterns
             System.out.println();
         }
     }
+}*/
+
+
+/*Hallow square
+    public class Pattern
+{
+    public static void main(String[] args)
+    {
+        int size = 5;
+
+        for (int i = 1; i <= size; i++)
+        {
+            for (int j = 1; j <= size; j++)
+            {
+                if (i == 1 || i == size || j == 1 || j == size)
+                {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+/*Plus symbol * patterns
+    public class Pattern
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        int mid = n / 2 + 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+               
+                if (i == mid || j == mid)
+                {
+                    System.out.print("* ");
+                } else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/* Cross symbol start patterns
+    public class Pattern {
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++)
+        {        
+            for (int j = 1; j <= n; j++)
+            {      
+               
+                if (i == j || i + j == n + 1)
+                {
+                    System.out.print("* ");
+                } else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/*Rhombus
+    public class Pattern {
+    public static void main(String[] args)
+    {
+        int rows = 4;
+        for (int i = 1; i <= rows; i++)
+        {
+
+            for (int j = 1; j <= rows - i; j++)
+            {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= rows - i; j++)
+            {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
 }
+*/
